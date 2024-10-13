1eb569399e4bd168b35b6ca4083b3b34db157daa
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-10-13 13:15:44 +0200

    build working

--- contracts/src/Popkorn2.ts
+++ contracts/src/Popkorn2.ts
@@ -63,7 +63,10 @@ import {
     }
   
     @method async addSigner(signerPubKey: PublicKey, witness: MerkleMapWitness) {
-      this.isInitialized.get().assertTrue();
+
+      const initialized = this.isInitialized.get();
+      this.isInitialized.requireEquals(initialized);
+      initialized.assertTrue();
   
       const currentRoot = this.signersMapRoot.get();
       this.signersMapRoot.requireEquals(currentRoot);
@@ -81,8 +84,11 @@ import {
     }
   
     @method async removeSigner(signerPubKey: PublicKey, witness: MerkleMapWitness) {
-      this.isInitialized.get().assertTrue();
-  
+
+      const initialized = this.isInitialized.get();
+      this.isInitialized.requireEquals(initialized);
+      initialized.assertTrue();
+
       const currentRoot = this.signersMapRoot.get();
       this.signersMapRoot.requireEquals(currentRoot);
       const currentCount = this.signersCount.get();
@@ -97,13 +103,17 @@ import {
       this.signersMapRoot.set(rootAfter);
       this.signersCount.set(currentCount.sub(1));
   
+      this.threshold.requireEquals(this.threshold.get());
       const currentThreshold = this.threshold.get();
-      currentThreshold.assertLessThanOrEqual(this.signersCount.get());
+      const signerCount = this.signersCount.get();
+      currentThreshold.assertLessThanOrEqual(signerCount);
     }
   
     @method async setThreshold(newThreshold: UInt64) {
-      this.isInitialized.get().assertTrue();
-  
+      const initialized = this.isInitialized.get();
+      this.isInitialized.requireEquals(initialized);
+      initialized.assertTrue();
+
       const currentCount = this.signersCount.get();
       this.signersCount.requireEquals(currentCount);
   
@@ -113,39 +123,44 @@ import {
       this.threshold.set(newThreshold);
     }
   
-    @method async interact(rootUpdate: AccountUpdateDescr, signatures: Signature[], signerPubKeys: PublicKey[]) {
-      this.isInitialized.get().assertTrue();
-  
+    @method async interact(
+      rootUpdate: AccountUpdateDescr, 
+      signature: Signature, 
+      signerPubKey: PublicKey
+    ) {
+
+      const initialized = this.isInitialized.get();
+      this.isInitialized.requireEquals(initialized);
+      initialized.assertTrue();
+
       const currentNonce = this.nonce.get();
       this.nonce.requireEquals(currentNonce);
       
       const currentThreshold = this.threshold.get();
       this.threshold.requireEquals(currentThreshold);
+
+
+      // TODO: 
+      // this.verifySignature(signature, signerPubKey, currentNonce);
+      // TODO: mark pubkey as approved
+      // TODO: check if threshold is reached
+      // TODO: if so, execute the interaction
   
-      this.verifySignatures(signatures, signerPubKeys, currentNonce);
+      // this.verifySignatures(signatures, signerPubKeys, currentNonce);
   
-      rootUpdate.apply(this.self);
+      // rootUpdate.apply(this.self);
       
-      this.nonce.set(currentNonce.add(1));
+      // this.nonce.set(currentNonce.add(1));
     }
   
-    verifySignatures(signatures: Signature[], signerPubKeys: PublicKey[], nonce: Field) {
-      const signaturesLength = signatures.length;
-      const pubKeysLength = signerPubKeys.length;
+    verifySignature(signature: Signature, signerPubKey: PublicKey, nonce: Field) {
+      // Verify the signature
+      signature.verify(signerPubKey, [nonce]).assertTrue();
       
-      Bool(signaturesLength === pubKeysLength).assertTrue();
-      UInt64.from(signaturesLength).assertGreaterThanOrEqual(this.threshold.get());
-  
-      for (let i = 0; i < signaturesLength; i++) {
-        const signature = signatures[i];
-        const publicKey = signerPubKeys[i];
-        
-        signature.verify(publicKey, [nonce]).assertTrue();
-        
-        const signerHash = Poseidon.hash(publicKey.toFields());
-        const witness = new MerkleMap().getWitness(signerHash);
-        const [root] = witness.computeRootAndKey(Field(1));
-        root.assertEquals(this.signersMapRoot.get());
-      }
+      // Verify that the signer is in the signers map
+      const signerHash = Poseidon.hash(signerPubKey.toFields());
+      const witness = new MerkleMap().getWitness(signerHash);
+      const [root] = witness.computeRootAndKey(Field(1));
+      root.assertEquals(this.signersMapRoot.get());
     }
   }
